7e0820ce484385526c7bd0bf3111592aeb3ee072
Falcinspire <Masteroreo77@users.noreply.github.com>
2019-06-27 23:59:20 -0400

    log calculator

---
+++ src/main/kotlin/com/falcinspire/hacklib/math/LogCalculator.kt
@@ -0,0 +1,7 @@
+fun log2floor(number: Int): Int {
+    // find the first 1 bit from the MSD side to the LSD side
+    for (counter in 0 until 32) {
+        if ((number and (1 shl (31 - counter))) > 0) return (31 - counter)
+    }
+    return -1
+}
