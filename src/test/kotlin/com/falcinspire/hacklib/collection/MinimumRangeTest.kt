75d0ed488c18fc3579bc45abced2102d58f5cdaa
Falcinspire <Masteroreo77@users.noreply.github.com>
2019-06-28 00:04:13 -0400

    minimum range test

---
+++ src/test/kotlin/com/falcinspire/hacklib/collection/MinimumRangeTest.kt
@@ -0,0 +1,9 @@
+import org.junit.Test
+
+class MinimumRangeTest {
+    @Test
+    fun test() {
+        val minrange = MinimumRange.create(intArrayOf(2, 4, 32, 10, 6, 7, 8, 9, 2, 7))
+        println(minrange[1, 8])
+    }
+}
